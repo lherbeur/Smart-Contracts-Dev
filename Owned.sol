5320416d2045f4a122295ff04fb3432e98fec7e0
GEORGE Olawunmi <contactlabo@yahoo.com>
2017-05-19 10:37:30 +0000

    Rough Owned draft

---
+++ Owned.sol
@@ -0,0 +1,26 @@
+pragma solidity^0.4.10;
+
+contract Owned{
+
+address owner;
+
+    function Owned() {
+
+       owner = msg.sender;
+    }
+
+
+function setOwner(address newOwner)
+isOwner
+{
+    owner = newOwner;
+}
+
+modifier isOwner()
+{
+    if (msg.sender != owner)
+        throw;
+    _;
+}
+
+}
