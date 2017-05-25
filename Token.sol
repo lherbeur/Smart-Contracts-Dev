54ffebe68c6c5c006630650ae552515fc590e8cb
Digicomme <Digicomme@bitbucket.org>
2017-05-25 02:08:56 +0100

    Implemented basic low level call function and fixed ERC23.sol relative path in token.sol

--- Token.sol
+++ Token.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.11;
 
 import "./Owned.sol";
-import "./ERC23.sol";
+import "./library/token/ERC23.sol";
 
 contract Token is Owned, ERC23  {
 	//conforming to the ERC20 /223 standard
@@ -14,7 +14,7 @@ contract Token is Owned, ERC23  {
 
 	/// @notice mapping to track amount of tokens each address holds
 	mapping (address => uint256) public balances;
-    
+
     /// @notice event triggered when tokens are transferred
     event Transfer(address indexed _from, address indexed _to, uint256 _value); //Transfer event
 
@@ -36,29 +36,29 @@ contract Token is Owned, ERC23  {
     function name() constant returns (string _name) {
         return name;
     }
-    
+
     /// @notice function to access symbol of token .
     function symbol() constant returns (string _symbol) {
         return symbol;
     }
-    
+
     /// @notice function to access decimals of token .
     function decimals() constant returns (uint8 _decimals) {
         return decimals;
     }
-    
+
     /// @notice function to access total supply of tokens .
     function totalSupply() constant returns (uint256 _totalSupply) {
         return totalSupply;
     }
-    
+
     function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
       }
-    
-    
+
+
     /**
-    * @notice function that is called when a user or another contract wants to transfer funds 
+    * @notice function that is called when a user or another contract wants to transfer funds
     * @dev ERC23 version of transfer
     * @param _to address where token will be sent
     * @param _value amount of tokens
@@ -71,17 +71,17 @@ contract Token is Owned, ERC23  {
 
 
     /**
-    * @notice @notice function that is called when a user or another contract 
+    * @notice @notice function that is called when a user or another contract
     *  wants to transfer funds with no _data
     * @dev Standard function transfer similar to ERC20 transfer with no _data .
     *  added due to backwards compatibility reasons .
     * @param _to address where token will be sent
     * @param _value of tokens
-    */ 
+    */
     function transfer(address _to, uint256 _value) returns (bool success) {
     	//.......checks
     	//.....and other lines of code
-  	
+
     }
 
     /**
@@ -92,7 +92,7 @@ contract Token is Owned, ERC23  {
     */
     function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
     }
-    
+
 
     /**
     * @dev function that is called when target address is external
@@ -102,14 +102,14 @@ contract Token is Owned, ERC23  {
     */
     function transferToAddress(address _to, uint _value, bytes _data) private returns (bool success) {
     }
-    
+
 
     /**
-    * @dev this function assembles the given address bytecode, if the 
+    * @dev this function assembles the given address bytecode, if the
     *  bytecode exists, then _addr is a contract.
     * @param _addr address of either a conract or external account
     */
     function isContract(address _addr) private returns (bool is_contract) {
-    }  
+    }
 
 }
