595bc53151b74f33bc6e142dee6e74987e956497
precioustosin <precioustosin@hotmail.com>
2017-05-26 23:15:35 +0100

    created javascript test for 'getContract' function

--- test/consensusx.js
+++ test/consensusx.js
@@ -1,5 +1,5 @@
 //Abstraction for ConsensusX
-var ConsensusX = artifacts.require("ConsensusX").
+/*var ConsensusX = artifacts.require("ConsensusX").
 address;
 
 contract('ConsensusX', function(){
@@ -9,5 +9,21 @@ contract('ConsensusX', function(){
 		}).then(function(address){
 			assert.equal(address.valueOf(), 0x0, "null address must be returned");
 		});
+	});
+});*/
+
+var Token = artifacts.require("Token");
+var ConsensusX = artifacts.require("ConsensusX");
+
+contract('ConsensusX', function(){
+	it("should find address", function(){
+		return Token.deployed().then(function(address){
+			return ConsensusX.deployed().then(function(instance){
+				return instance.getContracts.call("tosin");
+		}).then(function(address){
+			assert.equal(address.valueOf(), 0x0, "null address must be returned");
+		});
+		});
+
 	});
 });
