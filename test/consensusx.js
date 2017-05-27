f6d559e420be8c73a28124f634b2d03697a73e3f
precioustosin <precioustosin@hotmail.com>
2017-05-27 07:56:25 +0100

    added second test

--- test/consensusx.js
+++ test/consensusx.js
@@ -20,10 +20,19 @@ contract('ConsensusX', function(){
 		return Token.deployed().then(function(address){
 			return ConsensusX.deployed().then(function(instance){
 				return instance.getContracts.call("tosin");
-		}).then(function(address){
-			assert.equal(address.valueOf(), 0x0, "null address must be returned");
+			}).then(function(address){
+				assert.equal(address.valueOf(), 0x0, "null address must be returned");
+			});
 		});
-		});
+	});
 
+	it("it should add contract", function(){
+		return Token.deployed().then(function(address){
+			return ConsensusX.deployed().then(function(instance){
+				return instance.addContract.call("tosin", "0x7ca8fb59a4959251798fd85718366da82b726087");
+			}).then(function(bool){
+				assert.equal(bool, true, "true value must be returned");
+			});
+		});
 	});
 });
