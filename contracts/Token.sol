0bb6fcc33d8fcda7fc6613c4f8376c93e9fbbaae
Michael Aigboeghian <micki1.com@gmail.com>
2017-05-29 19:19:16 +0000

    Added 'isContract' function to check if given address is a contract address or wallet address

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -55,6 +55,21 @@ contract Token is Owned, ERC23  {
     function balanceOf(address _owner) constant returns (uint balance) {
         return balances[_owner];
     }
+    
+    //assemble the given address bytecode. If bytecode exists then the _addr is a contract.
+    function isContract(address _addr) private returns (bool is_contract) {
+        uint length;
+        assembly {
+            //retrieve the size of the code on target address, this needs assembly
+            length := extcodesize(_addr)
+        }
+        if(length>0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 
     /**
