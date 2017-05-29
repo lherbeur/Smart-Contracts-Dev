fd96eb1e409d96cc7c69d93d94b74fe1d03a49b1
Digicomme <Digicomme@bitbucket.org>
2017-05-29 13:31:47 +0100

    Entire vesting contract updates

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,88 +1,74 @@
-/*
-Copyright SuperDAO Prep May 2017
-*/
-
 pragma solidity ^0.4.11;
 
-import "./ConsensusX.sol";
+import "Owned.sol";
 
 
-/** @title Vesting
-* @author Charles
-* @author Wunmi
-* @author Tosin
-* @author Michael
-* @author Emmanuel
-* @author Promise
-*/
-contract Vesting {
-    /* address consX;
-    struct Schedule {
-        uint64 cliffBegin;
-        uint64 cliffEnd;
-        uint256 tokenStake;
+contract Vesting is Owned {
+
+
+    function Vesting (address _consensusX) {
+        consensusXAddr = _consensusX;
+    }
+
+    address public consensusXAddr;
+
+    struct personaSchedule {
+        bytes32 name;
+        uint64 start;
+        uint64 end;
+        uint64 cliff;
+        uint256 value;
         uint256 loggedBy;
     }
-    mapping (address => Schedule[]) public masterSchedule;
-    event Scheduled(address addr, uint64 cliffBegin, uint64 cliffEnd, uint256 tokenStake, uint256 loggedBy);
 
-    function Vesting() {
+    mapping (address => personaSchedule[]) public schedules;
 
-    }
+    event Scheduled(bytes32, address addr, uint64 start, uint64 end, uint256 value, uint256 loggedBy);
 
-
-    /**
-    * @dev assigns a vesting schedule to a persona address
-    * @param _addr - persona address 
-    * @param _cliffBegin - cliff start date
-    * @param _cliffEnd - cliff end date 
-    * @param _tokenStake - amount of tokens 
-    */
-    function assignSchedule(address _addr, uint64 _cliffBegin, uint64 _cliffEnd, uint256 _tokenStake) OnlyConsensusX returns (bool) {
-        Schedule[] schedules = masterSchedule[_addr];// extract previous persona schedule(s)
-        schedules.push(Schedule(_cliffBegin, _cliffEnd, _tokenStake));// push new schedule into schedules
-        masterSchedule[_addr] = schedules;// save updated schedules
-
-        Scheduled(_addr, _cliffBegin, _cliffEnd, _tokenStake, now);
-
-        return (masterSchedule[_addr] == 0);
-    }
-
-
-    /**
-    * @dev sets address of ConsensusX 
-    * @param _consXAddr - address of ConsensusX
-    */
-    function setConsXAddress(address _consXAddr) returns (bool){
-        if(consX != 0x0 && _consXAddr != consX){
-            return false;
-        }
-        consX = _consXAddr;
-        return true;
-    }
-
-    /**
-    * @dev checks if caller is ConsensusX
-    * @return true if the caller is ConsensusX 
-    */
-    function isConsXEnabled() internal constant returns (bool) {
-        if(consX != 0x0){
-            address ca = new ConsensusX(consX);
-            if (msg.sender == ca){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    modifier OnlyConsensusX() {
-        require(isConsXEnabled());
+    modifier onlyConsensusX () {
+        require(msg.sender == consensusXAddr);
         _;
     }
 
+    modifier checkNewScheduleData (
+        uint64 _start,
+        uint64 _end,
+        uint64 _cliff,
+        uint64 _value) {
+         require(
+             _start < _end
+             && _cliff > _start
+             && _cliff < _end
+             && _value > 0);
+         _;
+    }
 
-    // Fallback
-    function () {
-        throw;
-    } */
+    function assignSchedule (
+        address _to,
+        bytes32 _name,
+        uint64 _start,
+        uint64 _end,
+        uint64 _cliff,
+        uint64 _value)
+        onlyConsensusX
+        checkNewScheduleData(_start, _end, _cliff, _value)
+        returns (uint) {
+        uint n = schedules[_to].push(personaSchedule(
+            _name,
+            _start,
+            _end,
+            _cliff,
+            _value,
+            now
+            ));
+        require(n > 0);
+        Scheduled(_name, _to, _start, _end, _value, now);
+        return n;
+
+    }
+
+    function setConsensusXAddr (address _newConsensusXAddr) onlyConsensusX {
+        consensusXAddr = _newConsensusXAddr;
+    }
+
 }
