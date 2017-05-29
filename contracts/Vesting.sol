b18dcbd2335a38102f4a127495cc900f7ef27a4f
Olawunmi George <contactlabo@yhoo.com>
2017-05-29 21:07:51 +0100

    Vesting - modified codes based on style guide, function to get vesting schedules of persona, natspec comments

--- contracts/Vesting.sol
+++ contracts/Vesting.sol
@@ -1,18 +1,24 @@
+/*
+Copyright SuperDAO Prep May 2017
+*/
+
 pragma solidity ^0.4.11;
 
 import "Owned.sol";
 
-
+/** @title Vesting
+* @author Charles
+* @author Wunmi
+* @author Tosin
+* @author Michael
+* @author Emmanuel
+* @author Promise
+*/
 contract Vesting is Owned {
-
-
-    function Vesting (address _consensusX) {
-        consensusXAddr = _consensusX;
-    }
 
     address public consensusXAddr;
 
-    struct personaSchedule {
+    struct PersonaSchedule {
         bytes32 name;
         uint64 start;
         uint64 end;
@@ -21,9 +27,95 @@ contract Vesting is Owned {
         uint256 loggedBy;
     }
 
-    mapping (address => personaSchedule[]) public schedules;
+    mapping (address => PersonaSchedule[]) public schedules;
 
     event Scheduled(bytes32, address addr, uint64 start, uint64 end, uint256 value, uint256 loggedBy);
+
+
+    function Vesting (address _consensusX) {
+        consensusXAddr = _consensusX;
+    }
+
+    /**
+    * @notice Add name = `_name`, value = `_value`, start/end date = `_start` / `_end`,  cliff = `_cliff` to `_to`
+    * @dev assigns a vesting schedule to a persona address
+    * @param _to - persona address
+    * @param _name - name of schedule
+    * @param _start - start date
+    * @param _end - end date
+    * @param _cliff - cliff
+    * @param _value - amount of tokens
+    */
+    function assignSchedule (
+        address _to,
+        bytes32 _name,
+        uint64 _start,
+        uint64 _end,
+        uint64 _cliff,
+        uint64 _value)
+        onlyConsensusX
+        checkNewScheduleData(_start, _end, _cliff, _value)
+        returns (uint) {
+        uint n = schedules[_to].push(PersonaSchedule(
+            _name,
+            _start,
+            _end,
+            _cliff,
+            _value,
+            now
+            ));
+        require(n > 0);
+        Scheduled(_name, _to, _start, _end, _value, now);
+        return n;
+
+    }
+
+    /**
+    * @notice Get vesting schedule of `_personaAddr` at index `index`
+    * @dev gets a vesting schedule, with specified index, of a persona address
+    * @param _personaAddr - persona address
+    * @param index - index of schedules to get
+    * @return name, start date, end date, cliff, value and time schedule was added
+    */
+    function getVestingSchedulePerIndex (address _personaAddr, uint index)
+    onlyConsensusX
+    constant
+    returns (bytes32, uint64, uint64, uint64, uint256, uint256)
+    {
+        PersonaSchedule personaSchedule = schedules[_personaAddr][index];
+        return(
+        personaSchedule.name,
+        personaSchedule.start,
+        personaSchedule.end,
+        personaSchedule.cliff,
+        personaSchedule.value,
+        personaSchedule.loggedBy
+        );
+    }
+
+    /**
+    * @notice Get number of schedules for `_personaAddr`
+    * @dev gets the number of vesting schedules assigned to a persona
+    * @param _personaAddr - persona address
+    * @return  number of vesting schedules assigned to a persona
+    */
+    function getVestingScheduleCount (address _personaAddr)
+    onlyConsensusX
+    constant
+    returns (uint256)
+    {
+        return schedules[_personaAddr].length;
+    }
+
+    /**
+    * @notice Set  consensusXAddr to `_newConsensusXAddr`
+    * @dev Sets consensusXAddr to a new address
+    * @param _newConsensusXAddr - new address for consensusXAddr
+    */
+    function setConsensusXAddr (address _newConsensusXAddr) onlyConsensusX {
+        consensusXAddr = _newConsensusXAddr;
+    }
+
 
     modifier onlyConsensusX () {
         require(msg.sender == consensusXAddr);
@@ -41,34 +133,6 @@ contract Vesting is Owned {
              && _cliff < _end
              && _value > 0);
          _;
-    }
-
-    function assignSchedule (
-        address _to,
-        bytes32 _name,
-        uint64 _start,
-        uint64 _end,
-        uint64 _cliff,
-        uint64 _value)
-        onlyConsensusX
-        checkNewScheduleData(_start, _end, _cliff, _value)
-        returns (uint) {
-        uint n = schedules[_to].push(personaSchedule(
-            _name,
-            _start,
-            _end,
-            _cliff,
-            _value,
-            now
-            ));
-        require(n > 0);
-        Scheduled(_name, _to, _start, _end, _value, now);
-        return n;
-
-    }
-
-    function setConsensusXAddr (address _newConsensusXAddr) onlyConsensusX {
-        consensusXAddr = _newConsensusXAddr;
     }
 
 }
