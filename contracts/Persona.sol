b08c4c6629b1f4edf15bb8d8c057f15eb991871f
precioustosin <precioustosin@hotmail.com>
2017-06-01 22:31:30 +0100

    created 'callToTransfer' function to call Token's 'transfer' function through ConsensusX

--- contracts/Persona.sol
+++ contracts/Persona.sol
@@ -3,5 +3,17 @@ pragma solidity ^0.4.11;
 import "./ConsensusX.sol";
 
 contract Persona {
+    address Consensus;
+    function Persona(address _Consensus){
+        Consensus =  _Consensus;
+    }
+    
+    function CallToTransfer(address _to, uint _value){
+        ConsensusX(Consensus).callTransfer(_to, _value);
+    }
+    
+    function returnPersonaBalance() constant returns (uint){
+        return ConsensusX(Consensus).balances(this);
+    }
 	
 }
