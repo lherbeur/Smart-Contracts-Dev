ce68f55c3003f683a5c338dc5df36f635ce20355
precioustosin <precioustosin@hotmail.com>
2017-06-01 22:27:57 +0100

    removed token contract from contracts to be deployed seperately

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var ConsensusX = artifacts.require("./ConsensusX.sol");
-var Token = artifacts.require("./Token.sol");
+//var Token = artifacts.require("./Token.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.deploy(Token);
+  //deployer.deploy(Token);
   deployer.link(ConvertLib, MetaCoin);
-  deployer.link(Token, ConsensusX);
+  //deployer.link(Token, ConsensusX);
   deployer.deploy(MetaCoin);
   deployer.deploy(ConsensusX);
 };
