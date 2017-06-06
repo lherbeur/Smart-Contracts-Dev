d13dab7ece3c87e0c7406621b7d069026f6b9d1f
precioustosin <precioustosin@hotmail.com>
2017-06-07 00:54:45 +0100

    moved migration and deployment of ConsensusX and Persona contracts to new migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,8 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
-var ConsensusX = artifacts.require("./ConsensusX.sol");
-//var Token = artifacts.require("./Token.sol");
-
+																																																																																																																			
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  //deployer.deploy(Token);
   deployer.link(ConvertLib, MetaCoin);
-  //deployer.link(Token, ConsensusX);
   deployer.deploy(MetaCoin);
-  deployer.deploy(ConsensusX);
 };
