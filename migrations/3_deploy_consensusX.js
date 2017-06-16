52b76be45efa682fce70f6c60ecf16154eccc70f
precioustosin <precioustosin@hotmail.com>
2017-06-16 14:10:36 +0100

    added code to deploy TokenWallet following ERC223 standard and another wallet(FalseWallet) that does not

--- migrations/3_deploy_consensusX.js
+++ migrations/3_deploy_consensusX.js
@@ -1,7 +1,11 @@
 var ConsensusX = artifacts.require("./ConsensusX.sol");
 var Persona = artifacts.require("./Persona.sol");
+var TokenWallet = artifacts.require("./library/TokenWallet.sol");
+var FalseWallet = artifacts.require("./library/FalseWallet.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(TokenWallet);	
+  deployer.deploy(FalseWallet);	
   deployer.deploy(ConsensusX, "Pokerium", 0.00010, "POK", "1.0", 3000000).then(function(){
   	return deployer.deploy(Persona, ConsensusX.address);
   });
