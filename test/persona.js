a624f0f6c5c5947ce68a015ed8b7e4068f5eccc7
precioustosin <precioustosin@hotmail.com>
2017-06-23 18:05:24 +0100

    created another wallet for esting purposes

--- test/persona.js
+++ test/persona.js
@@ -4,7 +4,7 @@ var TokenWallet = artifacts.require("TokenWallet");
 
 //var consxInstance, personaAddr;
 
-contract('Persona', function(){
+contract('Persona', function(accounts){
 	
 	it("it should send total Tokens to a Persona Contract", function(){
 		return TokenWallet.deployed().then(function(){
@@ -20,14 +20,33 @@ contract('Persona', function(){
 	});
 
 	/*it("it should send Tokens to a Token Wallet", function(){
+		var personaAddr;
+		var balance
+		var wallet = TokenWallet.deployed();
+			return ConsensusX.deployed().then(function(instance){
+				var consxInstance = instance;
+				return Persona.deployed().then(function(instance){
+					personaAddr = instance;
+					consxInstance.allocateTokens.call(personaAddr);
+					return balance = personaAddr.returnPersonaBalance.call();
+				}).then(function(balance){
+					//console.log("From Persona, balance: " + balance);
+					//assert.equal(bool, true, "Transaction must return true");
+					//console.log(value);
+					//assert.equal(value.valueOf(), 3000000, "value must exist");
+				});
+			});
+	});*/
+
+	/*it("it should send Tokens to a Token Wallet", function(){
 		return TokenWallet.deployed().then(function(instance){
 			var wallet = instance.address;
 			return ConsensusX.deployed().then(function(instance){
 				var consxInstance = instance;
 				return Persona.deployed().then(function(instance){
 					consxInstance.allocateTokens.call(instance.address);
-					return instance.CallToTransfer.call(wallet, 100000);
-				}).then(function(){
+					return instance.CallToTransfer.call(wallet, 100000, {from:accounts[0],gas:4500000});
+				}).then(function(bool){
 					assert.equal(bool, true, "Transaction must return true");
 				});
 			});
