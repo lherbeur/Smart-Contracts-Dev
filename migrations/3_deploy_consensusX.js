c929a12a06f4105addc7043db8f481bea6f8c3c0
Adibas03 <adibas03@ymail.com>
2017-06-23 21:53:26 +0100

    Updated migrations for truffle tes, and added general test

--- migrations/3_deploy_consensusX.js
+++ migrations/3_deploy_consensusX.js
@@ -1,12 +1,23 @@
+var Owned = artifacts.require("./Owned.sol");
 var ConsensusX = artifacts.require("./ConsensusX.sol");
+var Token = artifacts.require("./Token.sol");
 var Persona = artifacts.require("./Persona.sol");
-var TokenWallet = artifacts.require("./library/TokenWallet.sol");
-var FalseWallet = artifacts.require("./library/FalseWallet.sol");
+var Erc23 = artifacts.require("./ERC23.sol");
+var TokenWallet = artifacts.require("./TokenWallet.sol");
+var FalseWallet = artifacts.require("./FalseWallet.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(TokenWallet);	
-  deployer.deploy(FalseWallet);	
+  //deployer.deploy(TokenWallet);
+  //deployer.deploy(FalseWallet);
+  //deployer.deploy(Erc23);
+  //deployer.deploy(Owned);
+  deployer.deploy([TokenWallet,FalseWallet,Erc23,Owned]);
+  deployer.link(Erc23,Token);
+  deployer.link(Owned,[ConsensusX,Token]);
+  deployer.deploy(Token);
+  deployer.link(Token,ConsensusX);
   deployer.deploy(ConsensusX, "Pokerium", 0.00010, "POK", "1.0", 3000000).then(function(){
+    deployer.link(ConsensusX,Persona);
   	return deployer.deploy(Persona, ConsensusX.address);
   });
 };
---
+++ test/do-tests.js
@@ -0,0 +1,53 @@
+var ConsensusX = artifacts.require("./ConsensusX"),
+ Persona = artifacts.require("./Persona"),
+ Token = artifacts.require("./Token"),
+ TokenWallet = artifacts.require("./TokenWallet"),
+ consxInstance, toknInstance, persona1;
+
+//var consxInstance, personaAddr;
+
+contract('All Tests', function(accounts){
+
+  it("Should deploy all contracts", function(){
+    return ConsensusX.deployed().then(function(instance){
+      consxInstance = instance;
+      console.log("ConsX deployed at: "+consxInstance.address);
+      return Token.deployed().then(function(tinstance){
+        toknInstance = tinstance;
+        console.log("Token deployed at: "+tinstance.address);
+        return Persona.deployed().then(function(pinstance){
+          persona1 = pinstance;
+          console.log("Persona 1 deployed at: "+persona1.address);
+        });
+      });
+    });
+  });
+
+  it("Should fetch Token total Supply", function(){
+    return toknInstance.totalSupply.call()
+    .then(function(a){
+      console.log( "Total token supply:",Number(a));
+    })
+    .then(function(){
+      return consxInstance.totalSupply.call()
+      .then(function(a){
+      console.log( "ConsX Total token supply:",Number(a));
+      });
+    });
+  });
+
+  /*
+	it("it should send total Tokens to a Persona Contract", function(){
+		return TokenWallet.deployed().then(function(){
+			return ConsensusX.deployed().then(function(instance){
+				var consxInstance = instance;
+        console.success("ConsX deployed at: "+consxInstance);
+				return Persona.deployed().then(function(pinstance){
+          persona1 = pinstance;
+          console.success("ConsX deployed at: "+consxInstance);
+			})
+		})
+	});
+  */
+
+});
