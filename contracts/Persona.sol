576848aec2516bac6c9a75dfdfb22e96a01a59c0
precioustosin <precioustosin@hotmail.com>
2017-09-12 13:03:52 +0100

    initial draft of persona contract

--- contracts/Persona.sol
+++ contracts/Persona.sol
@@ -1,11 +1,38 @@
 pragma solidity ^0.4.11;
 
+import "./Owned.sol";
 import "./ConsensusX.sol";
 
-contract Persona {
+contract Persona is Owned {
+
     address public personaOwner;
+    struct Token {
+        string name;
+        string symbol;
+        address tokenAddress;
+        uint value;
+    } 
     
-    function Persona(address _Consensus){
-        personaOwner = msg.sender;
-   }     
+    mapping (bytes8 => Token) public tokenMapping;
+    uint8[] public tokenKeys;
+    
+    function Persona(address ConsensusxAddr) {
+        personaOwner = ConsensusxAddr;
+    }
+    
+    function addTokens(address tokenAddr) {
+        
+    }
+    
+    function removeTokens(address tokenAddr) {
+        
+    }
+    
+    function doesTokenExist(bytes8 symbol) {
+        
+    }
+
+    function() {
+        throw;
+    }
 }
