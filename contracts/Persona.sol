a0dbb2edcd90d98c67fcec3349c6ddb8f50e475c
precioustosin <precioustosin@hotmail.com>
2017-09-15 10:29:18 +0100

    created three functions to transfer tokens based on number of arguments supplied, added comments, created function to return function signature, added more items to token struct

--- contracts/Persona.sol
+++ contracts/Persona.sol
@@ -9,57 +9,95 @@ contract Persona is Owned {
     struct Token {
         string name;
         string symbol;
-        address tokenAddress;
+        address tokenAddress; 
+        address senderAddress;
+        address txOriginAddress;
         uint value;
         mapping (address => bytes) data;
+        mapping (address => bytes4) sig;
     } 
-
-    mapping (bytes8 => address) public supportedTokens;//maps d SYM of d token & the token contract address
+    
+   mapping (bytes8 => address) public supportedTokens;//maps d SYM of d token & the token contract address
     //mapping (bytes8 => Token) public tokenMapping;
 
     uint8[] public tokenKeys;
-
+    
     event LogEtherSent(address recipient, uint amount, uint balance);
     event LogEtherReceived(address sender, uint amount, uint balance);
-
+    
     function Persona(address ConsensusxAddr) {
-
         personaOwner = ConsensusxAddr;
     }
-
-    function addToken(address tokenAddr) returns (bool) {
-
-    }
-
-    function removeToken(address tokenAddr) returns (bool) {
-
-    }
-
-    function modifyToken(address tokenAddr) returns (bool) {
-
-
+    
     function addToken(bytes8 tokenSymbol, address tokenAddr) returns (bool) {
-
       supportedTokens[tokenSymbol] = tokenAddr;
       return true;
     }
 
-    function removeToken(bytes32 tokenSymbol) returns (bool) {
-
+    function removeToken(bytes8 tokenSymbol) returns (bool) {
       delete supportedTokens[tokenSymbol];
       return true;
     }
-
+    
+    function modifyToken(address tokenAddr) returns (bool) {
+        
+    }
+    
     function doesTokenExist(bytes8 symbol) returns (bool) {
-
+        
     }
-
+    
     function sendEther(uint amount) returns (bool) {
-
+        
     }
-
+    
     /**
-    * @dev fallback function to be called (ERC223 standard)
+    * @notice function that is called when a user or another contract wants to 
+    *   transfer funds with no data
+    * @dev ERC23 version of transfer with no _data
+    * @param _to address where token will be sent
+    * @param _value amount of tokens
+    */
+    function transferToken(address _to, uint _value) returns (uint) {
+        //get instance of token using the token address
+        //run checks
+        //make contract state changes
+        //call token transfer function
+    }
+    
+    /**
+    * @notice function that is called when a user or another contract wants to 
+    *   transfer funds with _data
+    * @dev ERC23 version of transfer where _data is supplied
+    * @param _to address where token will be sent
+    * @param _value amount of tokens
+    * @param _data - information that accompanies transactions
+    */
+    function transferToken(address _to, uint _value, bytes _data) returns (uint) {
+        //get instance of token using the token address
+        //run checks
+        //make contract state changes
+        //call token transfer function
+    }
+    
+    /**
+    * @notice function that is called when a user or another contract wants to 
+    *   transfer funds with _data and _callback
+    * @dev ERC23 version of transfer where callback to handle tokens is supplied
+    * @param _to address where token will be sent
+    * @param _value amount of tokens
+    * @param _data - information that accompanies transactions
+    * @param _custom_fallback callback function
+    */
+    function transferToken(address _to, uint _value, bytes _data, string _custom_fallback) returns (uint) {
+        //get instance of token using the token address
+        //run checks
+        //make contract state changes
+        //call token transfer function
+    }
+    
+    /**  
+    * @dev fallback function to be called (ERC223 standard) 
     * @param _from address of oken sender
     * @param _value amount of Tokens sent
     * @param _data data sent with transaction
@@ -71,14 +109,21 @@ contract Persona is Owned {
         tkn.value = _value;
         tkn.data[_from] = _data;
     }
-
-    // constant getter functions
-    function displayEthBalance() constant returns (uint) {
-
+    
+    function getSignature(bytes _data) returns (bytes4 sig) {
+        uint32 u = uint32(_data[3]) + (uint32(_data[2]) << 8) + 
+            (uint32(_data[1]) << 16) + (uint32(_data[0]) << 24);
+        sig = bytes4(u);
     }
-
+    
+    // constant getter functions
+    
+    function displayEthBalance() constant returns (uint) {
+        
+    }
+    
     function displayTokenBalance(bytes32 symbol, address tokenAddress) constant returns (uint) {
-
+        
     }
 
     function() payable {
